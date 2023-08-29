8dacfd8180f7984dd3219f21c9c6ae380e7d3662
dasubermanmind <ero33@drexel.edu>
2023-08-29 13:45:57 -0400

    boilerplate

---
+++ models/node.py
@@ -0,0 +1,16 @@
+
+
+
+class Node:
+
+    def __init__(self):
+        pass
+
+    def create_node(self):
+        pass
+
+    def update_node(self, id):
+        pass
+
+    def delete_node(self, id):
+        pass
